078f1fb0c7ecfc9efa875a184494c8fc7ec2b8a5
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-08-05 23:24:14 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,7 +36,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    ##your code here
+    user = User.find_by(username: params[:username], password: params[:password])
   end
 
   get "/failure" do
